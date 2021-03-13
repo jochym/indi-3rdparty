db03751f1e5431d9ef02b20cfcd344419fea44f7
Fabrizio Pollastri <mxgbot@gmail.com>
2021-03-14 00:42:05 +0100

    Fix computation of north Az, add 12h to compensate Az and HA origin difference

--- indi-celestronaux/celestronaux.cpp
+++ indi-celestronaux/celestronaux.cpp
@@ -413,8 +413,9 @@ double CelestronAUX::getNorthAz()
     if (!GetDatabaseReferencePosition(location))
         northAz = 0.;
     else
-        northAz = AltAzFromRaDec(get_local_sidereal_time(location.lng), 0., 0.).az;
-    LOGF_DEBUG("North Azimuth = %lf", northAz);
+	// RA north = LST - HA north = LST + 12h
+        northAz = AltAzFromRaDec(get_local_sidereal_time(location.lng) + 12., 0., 0.).az;
+    LOGF_INFO("North Azimuth = %lf", northAz);
     return northAz;
 }
 
